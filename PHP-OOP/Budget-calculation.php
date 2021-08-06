f5decca00dc0093c611c79ec41f7a8cc35bdbad5
Majeed-hub <majeedbkl65@gmail.com>
2021-08-06 12:52:22 +0530

    programmed more lines of code for finance program

--- PHP-OOP/Budget-calculation.php
+++ PHP-OOP/Budget-calculation.php
@@ -6,6 +6,8 @@ class Budget
     private $expenses;
     private $tax;
     private $netIncome;
+    private $amountLeft;
+
 
     //50,30,20 budget philosophy
     public function __construct($income,$tax)
@@ -14,12 +16,49 @@ class Budget
         $this->tax = $tax;    
     }
 public function calculateNetIncome(){
-    $this->netIncome = $this->income * $this->tax / 100;
-         
-}
+        $this->amountLeft = $this->netIncome;
+
+    $tax = $this->income * $this->tax / 100;      
+     $this->netIncome = $this->income - $tax;
+        
+        return $this->netIncome;
+    }
 public function calculateSavings(){
      $this->savings = $this->netIncome * 0.20;
+     $this->netIncome -= $this->savings;
+
+     //assigning amount in another variable after calculating savings
+        $this->amountLeft = $this->netIncome;
+     return $this->savings;
 }
-public function calculateAffordability($price,$importance)
+public function calculateAffordability($price,$importance){
+    $affordability = ($price / $this->amountLeft)*100;
+    if($importance  && $affordability <= 50 ){
+            return "needs : you afford this item";
+    }else if( !$importance && $affordability <= 30 ){
+            return "wants : you afford this item";
+
+    }else {
+        return "you don't afford this item";
+    }
 }
+public function expense($amount){
+        $affordability = ($amount / $this->netIncome) * 100;
+        if ( $affordability <= 50 && $this->amountLeft != 0 ) {
+            return $this->amountLeft  -= $amount;
+        }
+}
+
+}
+$personalBudget = new Budget(100000,10);
+echo "your net income is = ". $personalBudget->calculateNetIncome()."<br>";
+echo "your saving money is = ". $personalBudget->calculateSavings() . "<br>" . "<br>";
+echo "affordability <br>".$personalBudget->calculateAffordability(20000,false) . "<br>";
+echo "amount left from your salary is = " . $personalBudget->expense(5000) . "<br>";
+echo "amount left from your salary is = " . $personalBudget->expense(10000) . "<br>";
+
+
+
+
+
 ?>
